dcbd3cde022b676275fc01a9d7cc3f3ea7c03cb8
Abu Anwar <abuanwar072@gmail.com>
2021-06-24 17:03:34 +0600

    validator added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 const primaryColor = Color(0xFFC41B09);
 
 const defaultPadding = 16.0;
+
+final MultiValidator emailValidator = MultiValidator(
+  [
+    RequiredValidator(errorText: "This Field is required"),
+    EmailValidator(errorText: "enter a valid email address")
+  ],
+);
+
+final passwordValidator = MultiValidator([
+  RequiredValidator(errorText: 'password is required'),
+  MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+  PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+      errorText: 'passwords must have at least one special character')
+]);
